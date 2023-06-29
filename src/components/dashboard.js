da121d20701abaf316e3399f00ad28244e25838a
unknown <greeshma.e@ahex.co.in>
2023-06-29 11:47:18 +0530

    task

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Dashboard = () => {
+  const [activeTab, setActiveTab] = useState('dashboard');
+
+  const handleTabClick = (tab) => {
+    setActiveTab(tab);
+  };
+
   return (
     <div className="dashboard">
-      {/* <h2>Dashboard</h2> */}
-      {/* Add dashboard content here */}
+      <button>products</button>  
+      <button>users</button>  
+
+      <button>rating</button>  
+
+      <button>total users</button>  
+
+
+      
     </div>
   );
 };
--- src/components/product.js
+++ src/components/product.js
@@ -3,13 +3,19 @@ import React from 'react';
 const Product = ({ productData }) => {
   return (
     <div>
+      
       {productData.map((product, index) => (
+        
         <div key={index} className="product-item">
-          <img src={product.productimage} alt={product.product_name} />
+          <img src={product.productimage} alt={product.product_name}/>
           <h3>{product.product_name}</h3>
+          <button className="add-to-cart" onClick={() => addToCart(product)}>
+            Add to Cart
+          </button>
+          <a href='/'></a>
         </div>
       ))}
-    </div>
+    </div>      
   );
 };
 
